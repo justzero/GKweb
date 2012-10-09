6a069fcd59506ed3570fc1f09d7a4eaa3337bc3f
justzero <gb.justzero@gmail.com>
2012-10-09 14:29:19 +0800

    fix

--- settings.py
+++ settings.py
@@ -57,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/var/'
+STATIC_ROOT = '/home/byteman/dev/web/web/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
